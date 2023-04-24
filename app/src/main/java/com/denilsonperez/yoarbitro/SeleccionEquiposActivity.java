7712afd9fc1c0c7cce931a7c42ab2215ed9bf90d
denilson1817 <denilson_peco@hotmail.com>
2023-04-24 13:04:34 -0600

    Seleccción de equipos

---
+++ app/src/main/java/com/denilsonperez/yoarbitro/SeleccionEquiposActivity.java
@@ -0,0 +1,14 @@
+package com.denilsonperez.yoarbitro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeleccionEquiposActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_equipos);
+    }
+}
