2f51f84059da48bc58bee3207037a9122383f7c1
denilson <denilson_peco@hotmail.com>
2023-05-25 17:54:26 -0600

    Registro de arbitros del partido funcionando

--- app/src/main/java/com/denilsonperez/yoarbitro/DatosArbitrosActivity.java
+++ app/src/main/java/com/denilsonperez/yoarbitro/DatosArbitrosActivity.java
@@ -5,14 +5,23 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DatosArbitrosActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     EditText nombreArbitroCentral, nombreArbitroAsistente1, nombreArbitroAsistente2;
     Button btnSiguiente, btnCancelar;
+    String nombreCentral, nombreAsistente1, nombreAsistente2 ,idJuego;
+    Intent recibir;
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference parentRef = firebaseDatabase.getReference("Cedulas");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,21 @@ public class DatosArbitrosActivity extends AppCompatActivity {
         btnSiguiente = findViewById(R.id.btnSiguiente);
         btnCancelar = findViewById(R.id.btnCancelar);
 
+        //recibir el id de la cedula
+        recibir = getIntent();
+        idJuego = recibir.getStringExtra("idJuego");
+
+        System.out.println("ID DE LA CEDULA: "+idJuego);
+
+
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarDatos();
+            }
+        });
+
         btnCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,5 +55,28 @@ public class DatosArbitrosActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void validarDatos() {
+        nombreCentral = nombreArbitroCentral.getText().toString();
+        if(TextUtils.isEmpty(nombreCentral)){
+            Toast.makeText(this, "ingrese un arbitro central", Toast.LENGTH_SHORT).show();
+        }else{
+            subirdatos();
+        }
+    }
+
+    private void subirdatos() {
+        parentRef.child(idJuego).child("Arbitros").child(nombreCentral).setValue("Central");
+
+        nombreAsistente1 = nombreArbitroAsistente1.getText().toString();
+        nombreAsistente2 = nombreArbitroAsistente2.getText().toString();
+        if(TextUtils.isEmpty(nombreAsistente1) && TextUtils.isEmpty(nombreAsistente2)){
+
+        } else{
+            parentRef.child(idJuego).child("Arbitros").child(nombreAsistente1).setValue("Asistente 1");
+            parentRef.child(idJuego).child("Arbitros").child(nombreAsistente2).setValue("Asistente 2");
+        }
+
     }
 }
