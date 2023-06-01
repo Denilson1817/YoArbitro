f2c5cd5568ce518c3793f952300b1433e46f9925
Jorfuje <arbjorfuje@gmail.com>
2023-05-31 23:30:24 -0600

    Subir pdf

--- app/src/main/java/com/denilsonperez/yoarbitro/PdfActivity.java
+++ app/src/main/java/com/denilsonperez/yoarbitro/PdfActivity.java
@@ -20,6 +20,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.TextPaint;
@@ -28,19 +29,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PdfActivity extends AppCompatActivity {
+
     Button btnGenerarCedula;
     String tituloCedula = "Benemerita Escuela Normal Veracruzana";
     String fecha, hora, campo, idJuego,datosDelPartido, arbitrosDelPartido;
@@ -49,6 +60,9 @@ public class PdfActivity extends AppCompatActivity {
     //Strings para jugadores
     String nombre, nombresJugadores1;
     Intent recibir;
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,10 +103,10 @@ public class PdfActivity extends AppCompatActivity {
         Canvas canvas = page.getCanvas();
 
         //Dibujar la imagen
-        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.logo);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
         //Se escala la imagen para el tamaño
         bitmapEscala = Bitmap.createScaledBitmap(bitmap, 80, 80, false);
-        canvas.drawBitmap(bitmapEscala,368, 20, paint);
+        canvas.drawBitmap(bitmapEscala, 368, 20, paint);
         //Se pasa el titulo al pdf
         titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
         titulo.setTextSize(20);
@@ -100,33 +114,95 @@ public class PdfActivity extends AppCompatActivity {
         //Se pasa los datos del partido al pdf
         datosPartido.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
         datosPartido.setTextSize(15);
-        canvas.drawText(datosDelPartido,10, 200, datosPartido);
+        canvas.drawText(datosDelPartido, 10, 200, datosPartido);
         //Se pasa los datos de abitros al pdf
         arbitrosPartido.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
         arbitrosPartido.setTextSize(15);
-        canvas.drawText(arbitrosDelPartido,10, 250, arbitrosPartido);
+        canvas.drawText(arbitrosDelPartido, 10, 250, arbitrosPartido);
         //Datos del equipo1
         equipo1Partido.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
         equipo1Partido.setTextSize(15);
-        int y=300;
+        int y = 300;
         String[] arrEquipo1 = nombresJugadores1.split("\n");
-        for (int i=0;i<arrEquipo1.length;i++){
-            canvas.drawText(arrEquipo1[i],10,y,equipo1Partido);
+        for (int i = 0; i < arrEquipo1.length; i++) {
+            canvas.drawText(arrEquipo1[i], 10, y, equipo1Partido);
             y += 15;
         }
 
         pdfDocument.finishPage(page);
+        // ...
 
-        File file = new File(Environment.getExternalStorageDirectory(), "Cedula.pdf");
-        try{
-            pdfDocument.writeTo(new FileOutputStream(file));
-            Toast.makeText(this, "Cedula creada", Toast.LENGTH_SHORT).show();
-        }catch (Exception e){
+        //pdfDocument.finishPage(page);
+
+// Obtener la instancia de FirebaseStorage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+// Crear una referencia al archivo en Firebase Storage
+        //StorageReference storageRef = storage.getReference();
+        //StorageReference pdfRef = storageRef.child("archivos/pdf/cedula.pdf");
+
+        ///verificar
+        // Obtén una referencia al directorio en el que deseas almacenar los archivos PDF
+        StorageReference storageRef = storage.getReference().child("archivos/pdf");
+
+// Genera un nombre único para el archivo PDF utilizando la fecha y hora actual
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        String fileName = "cedula_" + timestamp + ".pdf";
+
+// Crea la referencia al archivo con el nombre dinámico
+        StorageReference pdfRef = storageRef.child(fileName);
+
+
+
+
+
+// Obtener el OutputStream para el archivo PDF
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            pdfDocument.writeTo(outputStream);
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+// Obtener los bytes del archivo PDF
+        byte[] pdfBytes = outputStream.toByteArray();
+
+// Subir el archivo a Firebase Storage
+        UploadTask uploadTask = pdfRef.putBytes(pdfBytes);
+        uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()) {
+                    // El archivo PDF se subió exitosamente
+                    Toast.makeText(PdfActivity.this, "Archivo PDF guardado en Firebase Storage", Toast.LENGTH_SHORT).show();
+                    // Aquí puedes realizar otras operaciones, como guardar la URL del archivo en Firestore
+                } else {
+                    // Ocurrió un error al subir el archivo PDF a Firebase Storage
+                    Toast.makeText(PdfActivity.this, "Error al guardar el archivo PDF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //pdfDocument.close();
+
+
+        //File file = new File(Environment.getExternalStorageDirectory(), "Cedula.pdf");
+        //try {
+        // pdfDocument.writeTo(new FileOutputStream(file));
+        //Toast.makeText(this, "Cedula creada", Toast.LENGTH_SHORT).show();
+        //} catch (Exception e) {
+        //  e.printStackTrace();
+        // }
+
         pdfDocument.close();
+
+
+
+
+
     }
+
+
 
     private boolean checkPermission() {
         int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(),WRITE_EXTERNAL_STORAGE);
